6d080c0f19ad83d0f8b5338fb70cc35313732a4e
Mohamed Fouad <m.elsayedfouad99@gmail.com>
2022-12-20 22:53:50 +0200

    create_home_screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Text(
+              'there is no weather 😔 start',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              'searching now 🔍',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
