97c29bd662c38974f44238a50216a01a2d3f997d
Mohamed Fouad <m.elsayedfouad99@gmail.com>
2022-12-21 12:21:07 +0200

    add_search_icon

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screens/search_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,6 +8,15 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return SearchScreen();
+                }));
+              },
+              icon: Icon(Icons.search)),
+        ],
         title: const Text('Weather App'),
       ),
       body: Center(
